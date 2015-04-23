6270578cd759d4933c870dbbf889cd8c0c5bbdff
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-04-23 10:41:45 +0430

    bashSetup.sh

---
+++ bashSetup.sh
@@ -0,0 +1,7 @@
+
+echo "# SystemC and AcessNoxim EnV" >> ~/.bashrc
+echo "export SYSTEMC_HOME=/usr/local/systemc231" >> ~/.bashrc
+echo "export SYSTEMC_HEADERS=$SYSTEMC_HOME/include" >> ~/.bashrc
+echo "export C_INCLUDE_PATH=$SYSTEMC_HEADERS" >> ~/.bashrc
+echo "export CPLUS_INCLUDE_PATH=$SYSTEMC_HEADERS" >> ~/.bashrc
+echo "ok, everything is written to .bashrc"
